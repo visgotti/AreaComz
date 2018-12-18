d36c0504bf9691b65fee11c0b4f97b691ee404ea
gotti <spacelyjoe@gmail.com>
2018-12-18 16:17:27 -0500

    Uses ChannelClient API in some of its own API, also added message codes to be used in communication between servers

--- src/core/Channel.ts
+++ src/core/Channel.ts
@@ -1,10 +1,12 @@
+import { ChannelClient } from './ChannelClient';
 
 import { getAreaId } from './../helpers/getAreaId';
+import { MESSAGE_CODE_LOOKUP, LEAVE_AREA_CODE_LOOKUP } from './../index';
 
 export class Channel {
     private dealerSocket: any;
     private clients: Array<any>;
-    private pendingStates: Map<string, any>;
+    private pendingStates: any;
     public areaId: string;
     public areaIndex: number;
 
@@ -13,31 +15,48 @@ export class Channel {
         this.areaId = getAreaId(areaIndex, gameId);
         this.areaIndex = areaIndex;
         this.clients = [];
-        this.pendingStates = new Map();
+        this.pendingStates = {};
     }
 
     public onAreaMessage(message) {}
 
-    public addClient(client) {
-        this.clients.push(client);
+    public addClient(sessionId: string) {
+        const channelClient = new ChannelClient(sessionId, this);
+        // attach channelClient to the client
+        this.clients.push(channelClient);
+        return channelClient;
     }
 
-    // TODO clean area change vs. unnatural removal/disconnection and must notify area serve
-    public removeClient(sessionId) {
+    /** Removes client completely from system, allows options to relay message to area server.
+     * @param sessionId
+     * @param reasonCode - if the removal came from the connector end we need to notify
+     * the area server with a disconnect message with a reason code.
+     * @param data - If you need to send extra data along about the removal
+     */
+    public removeClient(sessionId: string, reasonCode?: number, data?: any) {
         this.clients = this.clients.filter((_client) => {
             return _client.sessionId !== sessionId
         });
-        if(this.pendingStates.hasOwnProperty(sessionId)) {
-            delete this.pendingStates[sessionId];
+
+        delete this.pendingStates[sessionId];
+
+        if(reasonCode !== null && !(isNaN(reasonCode))) {
+            console.log('the reason code was', reasonCode);
+            if(!(LEAVE_AREA_CODE_LOOKUP[reasonCode])) {
+                throw 'Invalid reason code provided to removeClient';
+            }
+            this._sendClientDisconnect(sessionId, reasonCode, data);
         }
     }
 
     // this overrides any past state for the session as newest.
-    public updateClientStates(sessionId, data) {
-        if(!(sessionId in this.pendingStates)) {
-            this.pendingStates[sessionId] = [];
-        }
+    public updateClientState(sessionId, data) {
         this.pendingStates[sessionId] = data;
+    }
+
+    // used to get current state of player in channel
+    public getCurrentState(sessionId) {
+        return this.pendingStates[sessionId];
     }
 
     // use this if you dont want to wait to relay
@@ -57,5 +76,28 @@ export class Channel {
     public sendClientStates() {
         const serialized = JSON.stringify(this.pendingStates);
         this.dealerSocket.send([this.areaId, '', serialized])
+    }
+
+    /**
+     * @param sessionId - id of client to disconnect
+     * @param reasonCode - reason the channel is telling the area client disconnected
+     * @param data - optional data to send a long with the disconnect message
+     * @private
+     */
+    private _sendClientDisconnect(sessionId, reasonCode: number, data?: any) {
+        const msg : any = {
+            type: MESSAGE_CODE_LOOKUP.DISCONNECT,
+            reasonCode,
+        };
+
+        if(data) {
+            msg.data = data;
+        };
+
+        this.sendMessage({
+            type: MESSAGE_CODE_LOOKUP.DISCONNECT,
+            sessionId,
+            reasonCode,
+        });
     }
 };
--- src/core/Connector.ts
+++ src/core/Connector.ts
@@ -1,6 +1,9 @@
 import * as zmq from 'zeromq';
 
 import { Channel } from './Channel';
+import {ChannelClient} from './ChannelClient';
+
+import { LEAVE_AREA_CODE_LOOKUP } from './../index';
 
 export class Connector {
     private dealerSocket: any = {};
@@ -30,7 +33,58 @@ export class Connector {
     public onAreaMessage(areaId: string, message: any){}
 
     public getChannel(areaId) {
-        return this.channelMap[areaId];
+        return this.channelMap.get(areaId);
+    }
+
+    public changeClientArea(sessionId, newAreaIndex) : ChannelClient {
+        let channelClient = this._getChannelClient(sessionId);
+        if(channelClient) {
+            const channel = channelClient.getCurrentChannel();
+
+            if(channel.areaIndex === newAreaIndex) return null;
+
+            channel.removeClient(sessionId);
+
+            this._deleteChanelClient(sessionId);
+
+            return this.initializeClientChannel(sessionId, newAreaIndex);
+        }
+        return null;
+    }
+
+    public disconnectClient(sessionId) {
+        const channelClient = this._getChannelClient(sessionId);
+        if(channelClient) {
+            const channel = channelClient.getCurrentChannel();
+            channel.removeClient(sessionId, LEAVE_AREA_CODE_LOOKUP.CONNECTION_LOST);
+        }
+        this.sessionIdMap.delete(sessionId);
+    }
+
+    //TODO refactor to return a promise and maybe change the function name to connect
+    public initializeClientChannel(sessionId, areaIndex) : ChannelClient {
+        let channelClient: ChannelClient;
+
+        channelClient = this._getChannelClient(sessionId);
+
+        if (channelClient !== null) throw new Error('Client is already initialized, use changeClientArea if you are trying to change areas.');
+
+        if (this.channels[areaIndex]) {
+            this._addSessionIdToMap(sessionId);
+            channelClient = this.channels[areaIndex].addClient(sessionId);
+            const session = this.sessionIdMap.get(sessionId);
+            session.channelClient = channelClient;
+            return channelClient;
+        } else {
+            console.log('throwing');
+            throw new Error('The area index you passed in does not have a channel.');
+        }
+    }
+
+    public sendChannelStates() {
+        for (let i = 0; i < this.channels.length; i += 1) {
+            this.channels[i].sendClientStates();
+        }
     }
 
     public close() {
@@ -39,18 +93,17 @@ export class Connector {
     }
 
     private handleReceivedMessage(areaId: string, message: any) {
-
         message = JSON.parse(message);
         areaId = areaId.toString();
         this.onAreaMessage(areaId, message);
-        this.channelMap[areaId].onAreaMessage(message);
+        this.channelMap.get(areaId).onAreaMessage(message);
     }
 
     private createChannels(areasData: Array<any>) {
         for (let i = 0; i < areasData.length; i += 1) {
             const channel = new Channel(this.dealerSocket, i, this.gameId);
             this.channels.push(channel);
-            this.channelMap[channel.areaId] = channel;
+            this.channelMap.set(channel.areaId, channel);
             // connect to the area server's publisher uri
             this.subscriberSocket.connect(areasData[i].URI);
             // subscribe to handle messages for area index.
@@ -64,27 +117,29 @@ export class Connector {
         });
     }
 
-    private changeArea(sessionId, newAreaIndex) {
-        if (sessionId in this.sessionIdMap && this.sessionIdMap[sessionId].areaIndex !== newAreaIndex) {
-            this.sessionIdMap[sessionId].areaIndex = newAreaIndex;
-            this.channels[this.sessionIdMap[sessionId].areaIndex].removeClient(sessionId);
-            this.channels[newAreaIndex].addClient(this.sessionIdMap[sessionId].client);
+    private _addSessionIdToMap(sessionId) : any {
+        this.sessionIdMap.set(sessionId, {
+            sessionId
+        });
+
+        return this.sessionIdMap.get(sessionId);
+    }
+
+    private _deleteChanelClient(sessionId) {
+        const session = this.sessionIdMap.get(sessionId);
+        if(session) {
+            delete session.channelClient;
         }
     }
 
-    public addClient(client, areaIndex) {
-        this.sessionIdMap[client.sessionId] = {
-            client,
-            areaIndex,
-        };
-        if (this.channels[areaIndex]) {
-            this.channels[areaIndex].addClient(client);
+    private _getChannelClient(sessionId) : ChannelClient {
+        const session = this.sessionIdMap.get(sessionId);
+        if(session) {
+            const channelClient = session.channelClient;
+            if(channelClient) {
+                return channelClient;
+            }
         }
-    }
-
-    public sendChannelStates() {
-        for (let i = 0; i < this.channels.length; i += 1) {
-            this.channels[i].sendClientStates();
-        }
+        return null;
     }
 };
